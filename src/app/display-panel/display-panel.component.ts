cc310d7a57393868767e5be252d434205b8435bd
Nick <missourimayberry@gmail.com>
2022-11-01 15:16:39 -0500

    calculator practice

---
+++ src/app/display-panel/display-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-panel',
+  templateUrl: './display-panel.component.html',
+  styleUrls: ['./display-panel.component.css']
+})
+export class DisplayPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
